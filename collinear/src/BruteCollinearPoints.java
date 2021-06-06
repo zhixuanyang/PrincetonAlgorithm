a3188ae17d5ba631770915a7025a5f6c7d8cd108
zhixuanyang <yzx72497@gmail.com>
2021-06-06 17:29:14 +0800

    initialized hw3

--- collinear/src/BruteCollinearPoints.java
+++ collinear/src/BruteCollinearPoints.java
@@ -1,8 +1,9 @@
 import edu.princeton.cs.algs4.Queue;
+import java.util.Arrays;
 public class BruteCollinearPoints {
-    Queue<LineSegment> temp = new Queue<>();
+    private Queue<LineSegment> temp = new Queue<>();
     private int count;
-    LineSegment[] result;
+    private LineSegment[] result;
     public BruteCollinearPoints(Point[] points) {
         if (points == null) {
             throw new IllegalArgumentException();
@@ -11,20 +12,23 @@ public class BruteCollinearPoints {
             if (points[i] == null) {
                 throw new IllegalArgumentException();
             }
-            for (int j = 1; j < points.length; j++) {
+            for (int j = i + 1; j < points.length; j++) {
                 if (points[i].compareTo(points[j]) == 0) {
                     throw new IllegalArgumentException();
                 }
             }
         }
+        Point[] test = new Point[points.length];
+        System.arraycopy(points, 0, test, 0, points.length);
+        Arrays.sort(test);
         count = 0;
-        for (int i = 0; i < points.length - 3; i++) {
-            for (int j = 1; j < points.length - 2; j++) {
-                for (int k = 2; k < points.length - 1; k++) {
-                    for (int m = 3; m < points.length; m++) {
-                        if (points[i].slopeTo(points[j]) == points[i].slopeTo(points[k])
-                                & points[i].slopeTo(points[j]) == points[i].slopeTo(points[m])) {
-                            temp.enqueue(new LineSegment(points[i], points[m]));
+        for (int i = 0; i < test.length - 3; i++) {
+            for (int j = i + 1; j < test.length - 2; j++) {
+                for (int k = j + 1; k < test.length - 1; k++) {
+                    for (int m = k + 1; m < test.length; m++) {
+                        if (test[i].slopeTo(test[j]) == test[i].slopeTo(test[k])
+                                & test[i].slopeTo(test[j]) == test[i].slopeTo(test[m])) {
+                            temp.enqueue(new LineSegment(test[i], test[m]));
                             count += 1;
                         }
                     }
--- collinear/src/FastCollinearPoints.java
+++ collinear/src/FastCollinearPoints.java
@@ -1,8 +1,10 @@
 import java.util.Arrays;
-import java.util.ArrayList;
+import edu.princeton.cs.algs4.Queue;
+
 public class FastCollinearPoints {
     private int count;
-    LineSegment[] result;
+    private LineSegment[] result;
+    private Queue<LineSegment> store;
     public FastCollinearPoints(Point[] points) {
         if (points == null) {
             throw new IllegalArgumentException();
@@ -11,17 +13,55 @@ public class FastCollinearPoints {
             if (points[i] == null) {
                 throw new IllegalArgumentException();
             }
-            for (int j = 1; j < points.length; j++) {
+            for (int j = i + 1; j < points.length; j++) {
                 if (points[i].compareTo(points[j]) == 0) {
                     throw new IllegalArgumentException();
                 }
             }
         }
+        store = new Queue<>();
         count = 0;
         for (int i = 0; i < points.length - 3; i++) {
             Point[] temp = new Point[points.length - 1 - i];
             System.arraycopy(points, i, temp, 0, points.length - 1 - i);
             Arrays.sort(temp, points[i].slopeOrder());
+            for (int j = 3; j < temp.length; j++) {
+                if (temp[j].slopeTo(temp[j - 1]) == temp[j].slopeTo(temp[j - 2])) {
+                    if (temp[j].slopeTo(temp[j - 3]) == temp[j].slopeTo(temp[j - 1])) {
+                        int record = j;
+                        int number = 0;
+                        if (temp[j + 1] != null) {
+                            number += 1;
+                            while (temp[record].slopeTo(temp[record + number])
+                                    == temp[record].slopeTo(temp[record - 1])) {
+                                j += 1;
+                                number += 1;
+                            }
+                        }
+                        if (number > 0) {
+                            number = number - 1;
+                        }
+                        count += 1;
+                        store.enqueue(new LineSegment(points[record - 3], points[record + number]));
+                    }
+                } else {
+                    break;
+                }
+            }
         }
+    }
+
+    public int numberOfSegments() {
+        return count;
+    }
+
+    public LineSegment[] segments() {
+        result = new LineSegment[count];
+        int number = 0;
+        while (!store.isEmpty()) {
+            result[number] = store.dequeue();
+            number += 1;
+        }
+        return result;
     }
 }
