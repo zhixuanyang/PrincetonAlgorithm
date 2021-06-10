71779150e00d7f5e3d338faa02429f15a7faad93
zhixuanyang <yzx72497@gmail.com>
2021-06-11 01:53:07 +0800

    updated seam

--- SeamCarving/src/SeamCarver.java
+++ SeamCarving/src/SeamCarver.java
@@ -1,226 +1,215 @@
-import edu.princeton.cs.algs4.Picture;
-import java.awt.Color;
+import  edu.princeton.cs.algs4.Picture;
 public class SeamCarver {
-    private Picture picture;
-    private int width;
-    private int height;
-    private boolean isVertical = true;
+    public static void main(String[] args) {
+
+    }
     public SeamCarver(Picture picture) {
         if (picture == null) {
-            throw new IllegalArgumentException();
+            throw new java.lang.IllegalArgumentException();
         }
-        this.picture = new Picture(picture);
-        this.width = picture.width();
-        this.height = picture.height();
-    }
 
+        int width = picture.width();
+        int height = picture.height();
+        colors = new int[width][height];
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                colors[i][j] = picture.getRGB(i, j);
+            }
+        }
+    }
+    private int[][] colors;
     public Picture picture() {
+        Picture picture = new Picture(colors.length, colors[0].length);
+        for (int i = 0; i < colors.length; i++) {
+            for (int j = 0; j < colors[0].length; j++) {
+                picture.setRGB(i, j, colors[i][j]);
+            }
+        }
         return picture;
     }
 
     public int width() {
-        return width;
+        return this.colors.length;
     }
 
     public int height() {
-        return height;
+        return this.colors[0].length;
     }
 
-    public double energy(int x, int y) {
-        if (x >= width || x < 0 || y >= height || y < 0) {
-            throw new IllegalArgumentException();
+    public  double energy(int x, int y) {
+        if (x < 0 || x > this.width() - 1 || y < 0 || y > this.height() - 1) {
+            throw new java.lang.IllegalArgumentException();
         }
-        if (x == 0 || x == width() - 1 || y == 0 || y == height() - 1) {
+
+        if (x == 0 || x == this.width() - 1 || y == 0 || y == this.height() - 1) {
             return 1000.0;
         }
-        Color up, down, left, right;
-        if (isVertical) {
-            up = y > 0 ? picture.get(x, y - 1) : picture.get(x, height - 1);
-            down = y < height - 1 ? picture.get(x, y + 1) : picture.get(x, 0);
-            left = x > 0 ? picture.get(x - 1, y) : picture.get(width - 1, y);
-            right = x < width - 1 ? picture.get(x + 1, y) : picture.get(0, y);
-        } else {
-            up = x > 0 ? picture.get(x - 1, y) : picture.get(height - 1, y);
-            down = x < height - 1 ? picture.get(x + 1, y) : picture.get(0, y);
-            left = y > 0 ? picture.get(x, y - 1) : picture.get(x, width - 1);
-            right = y < width - 1 ? picture.get(x, y + 1) : picture.get(x, 0);
-        }
-        int rx = left.getRed() - right.getRed();
-        int gx = left.getGreen() - right.getGreen();
-        int bx = left.getBlue() - right.getBlue();
-        int ry = up.getRed() - down.getRed();
-        int gy = up.getGreen() - down.getGreen();
-        int by = up.getBlue() - down.getBlue();
-        return Math.sqrt(rx * rx + gx * gx + bx * bx + ry * ry + gy * gy + by * by);
+        return Math.sqrt(gradx(x, y) + grady(x, y));
+    }
+    // energy of pixel at column x and row y
+
+
+    private int gradx(int x, int y) {
+        int rgb1 = colors[x - 1][y];
+        int r1 = (rgb1 >> 16) & 0xFF;
+        int g1 = (rgb1 >>  8) & 0xFF;
+        int b1 = (rgb1 >>  0) & 0xFF;
+
+        int rgb2 = colors[x + 1][y];
+        int r2 = (rgb2 >> 16) & 0xFF;
+        int g2 = (rgb2 >>  8) & 0xFF;
+        int b2 = (rgb2 >>  0) & 0xFF;
+
+        int sum = (r1 - r2) * (r1 - r2)
+                + (g1 - g2) * (g1 - g2)
+                + (b1 - b2) * (b1 - b2);
+        return sum;
     }
 
-    public int[] findHorizontalSeam() {
-        isVertical = false;
-        swap();
-        int[] res = findVerticalSeam();
-        swap();
-        isVertical = true;
-        return res;
+    private int grady(int x, int y) {
+        int rgb1 = colors[x][y - 1];
+        int r1 = (rgb1 >> 16) & 0xFF;
+        int g1 = (rgb1 >>  8) & 0xFF;
+        int b1 = (rgb1 >>  0) & 0xFF;
+
+        int rgb2 = colors[x][y + 1];
+        int r2 = (rgb2 >> 16) & 0xFF;
+        int g2 = (rgb2 >>  8) & 0xFF;
+        int b2 = (rgb2 >>  0) & 0xFF;
+
+        int sum = (r1 - r2) * (r1 - r2)
+                + (g1 - g2) * (g1 - g2)
+                + (b1 - b2) * (b1 - b2);
+        return sum;
     }
 
-    private void swap() {
-        int temp = width;
-        width = height;
-        height = temp;
+
+
+    // sequence of indices for horizontal seam
+    public   int[] findHorizontalSeam() {
+        this.colors = transpose(this.colors);
+        int[] seam = findVerticalSeam();
+        this.colors = transpose(this.colors);
+        return seam;
     }
+    // sequence of indices for horizontal seam
 
-    public int[] findVerticalSeam() {
-        int[][] path = new int[width][height];
-        double[][] cost = new double[width][height];
-        for (int i = 0; i < width; i++) {
-            double e = isVertical ? energy(i, 0) : energy(0, i);
-            cost[i][0] = e;
-            path[i][height - 1] = i;
-        }
 
-        for (int j = 1; j < height; j++) {
-            for (int i = 0; i < width; i++) {
-                double e = isVertical ? energy(i, j) : energy(j, i);
-                cost[i][j] = e + getMinCost(i, j, path, cost);
-            }
-        }
-
-        int[] res = new int[height];
-        double min = Double.MAX_VALUE;
-        int minPos = 0;
-        for (int i = 0; i < width; i++) {
-            if (cost[i][height - 1] < min) {
-                min = cost[i][height - 1];
-                minPos = i;
-            }
-        }
-
-        for (int j = height - 1; j >= 0; j--) {
-            res[j] = path[minPos][j];
-            minPos = res[j];
-        }
-        return res;
-    }
-
-    private double getMinCost(int i, int j, int[][] path, double[][] cost) {
-        double[] v = new double[3];
-        v[1] = cost[i][j - 1];
-        if (i > 0) {
-            v[0] = cost[i - 1][j - 1];
-        } else {
-            v[0] = Double.MAX_VALUE;
-        }
-        if (i < width - 1) {
-            v[2] = cost[i + 1][j - 1];
-        } else {
-            v[2] = Double.MAX_VALUE;
-        }
-        double res = Double.MAX_VALUE;
-        int pos = 0;
-        for (int m = 0; m < 3; m++) {
-            if (v[m] < res) {
-                res = v[m];
-                pos = m;
-            }
-        }
-        path[i][j - 1] = pos + i - 1;
-        return res;
-    }
-
-    public void removeHorizontalSeam(int[] seam) {
-        if (seam == null || seam.length != height()) {
+    private int[][] transpose(int[][] org) {
+        if (org == null) {
             throw new IllegalArgumentException();
+        }
+        int[][] rvs = new int[org[0].length][org.length];
+        for (int x = 0; x < org[0].length; x++) {
+            for (int y = 0; y < org.length; y++) {
+                rvs[x][y] = org[y][x];
+            }
+        }
+        return rvs;
+    }
+
+
+    // sequence of indices for vertical seam
+    public int[] findVerticalSeam() {
+        //find smallest path via topological order
+        int n = colors.length * colors[0].length;
+        int[] vertexTo = new int[n];
+        double[] distTo = new double[n];
+
+        //set initial distance
+        for (int v = 0; v < n; v++) {
+            if (v < colors.length) {
+                distTo[v] = 0;
+            } else {
+                distTo[v] = Double.POSITIVE_INFINITY;
+            }
+        }
+
+        for (int j = 0; j < colors[0].length - 1; j++) {
+            for (int i = 0; i < colors.length; i++) {
+                for (int k = -1; k < 2; k++) {
+                    if ((i + k < colors.length) && (i + k >= 0)) {
+                        int v = i + j * colors.length; //from [i][j]
+                        int w = i + k + (j + 1) * colors.length; //to [i+k][j+1]
+                        double energy = energy(i + k, j + 1);
+                        if (distTo[w] > distTo[v] + energy) {
+                            distTo[w] = distTo[v] + energy;
+                            vertexTo[w] = v;
+                        }
+                    }
+                }
+            }
+        }
+
+        //generate int[] seam!
+        int[] seam = new int[colors[0].length];
+
+        //find the last element of seam[]
+        double min = distTo[(height() - 1) * width()];
+        int end = (height() - 1) * width();
+        for (int i = (height() - 1) * width(); i < height() * width(); i++) {
+            if (min >= distTo[i]) {
+                min = distTo[i];
+                end = i;
+            }
+        }
+
+        //get value of seam[] from what we built
+        seam[height() - 1] = end - (height() - 1) * width();
+        for (int i = height() - 2; i > -1; i--) {
+            end = vertexTo[end];
+            seam[i] = end - i * width();
+        }
+        return seam;
+    }
+    // sequence of indices for vertical seam
+
+
+    private int index(int x, int y) {
+        return width() * y + x;
+    }
+
+
+
+
+    //     remove horizontal seam from current picture
+    public    void removeHorizontalSeam(int[] seam) {
+        if (seam == null) {
+            throw new java.lang.IllegalArgumentException();
+        }
+
+        if (seam.length != width()) {
+            throw new java.lang.IllegalArgumentException();
         }
         for (int i = 0; i < seam.length; i++) {
             if (seam[i] < 0 || seam[i] > height() - 1) {
-                throw new IllegalArgumentException();
+                throw new java.lang.IllegalArgumentException();
             }
             if (i != seam.length - 1 && (seam[i] - seam[i + 1] > 1 || seam[i] - seam[i + 1] < -1)) {
-                throw new IllegalArgumentException();
+                throw new java.lang.IllegalArgumentException();
             }
         }
         if (height() <= 1) {
-            throw new IllegalArgumentException();
+            throw new java.lang.IllegalArgumentException();
         }
-        this.picture = removeHorizontalSeam(picture, seam);
+        int[][] update = new int[width()][height() - 1];
+        for (int j = 0; j < width(); j++) {
+            if (seam[j] == 0) {
+                System.arraycopy(this.colors[j], 1, update[j], 0, height() - 1);
+            } else if (seam[j] == height() - 1) {
+                System.arraycopy(this.colors[j], 0, update[j], 0, height() - 1);
+            } else {
+                System.arraycopy(this.colors[j], 0, update[j], 0, seam[j]);
+                System.arraycopy(this.colors[j], seam[j] + 1,
+                        update[j], seam[j], height() - seam[j] - 1);
+            }
+        }
+        this.colors = update;
     }
-
     public void removeVerticalSeam(int[] seam) {
-        if (seam == null || seam.length != width()) {
-            throw new IllegalArgumentException();
-        }
-        for (int i = 0; i < seam.length; i++) {
-            if (seam[i] < 0 || seam[i] > width() - 1) {
-                throw new IllegalArgumentException();
-            }
-            if (i != seam.length - 1 && (seam[i] - seam[i + 1] > 1 || seam[i] - seam[i + 1] < -1)) {
-                throw new IllegalArgumentException();
-            }
-        }
-        if (width() <= 1) {
-            throw new IllegalArgumentException();
-        }
-        this.picture = removeVerticalSeam(picture, seam);
-    }
-    private Picture removeHorizontalSeam(Picture var0, int[] var1) {
-        if (var1 == null) {
-            throw new NullPointerException("Input seam array cannot be null.");
-        } else if (var0.width() == 1) {
-            throw new IllegalArgumentException("Image width is 1.");
-        } else if (var1.length != var0.width()) {
-            throw new IllegalArgumentException("Seam length does not match image width.");
-        } else {
-            for (int var2 = 0; var2 < var1.length - 2; ++var2) {
-                if (Math.abs(var1[var2] - var1[var2 + 1]) > 1) {
-                    throw new IllegalArgumentException("Invalid seam, "
-                            + "consecutive vertical indices are greater than one apart.");
-                }
-            }
-
-            Picture var5 = new Picture(var0.width(), var0.height() - 1);
-
-            for (int var3 = 0; var3 < var0.width(); ++var3) {
-                int var4;
-                for (var4 = 0; var4 < var1[var3]; ++var4) {
-                    var5.set(var3, var4, var0.get(var3, var4));
-                }
-
-                for (var4 = var1[var3] + 1; var4 < var0.height(); ++var4) {
-                    var5.set(var3, var4 - 1, var0.get(var3, var4));
-                }
-            }
-            return var5;
-        }
-    }
-
-    private Picture removeVerticalSeam(Picture var0, int[] var1) {
-        if (var1 == null) {
-            throw new NullPointerException("Input seam array cannot be null.");
-        } else if (var0.height() == 1) {
-            throw new IllegalArgumentException("Image height is 1.");
-        } else if (var1.length != var0.height()) {
-            throw new IllegalArgumentException("Seam length does not match image height.");
-        } else {
-            for (int var2 = 0; var2 < var1.length - 2; ++var2) {
-                if (Math.abs(var1[var2] - var1[var2 + 1]) > 1) {
-                    throw new IllegalArgumentException("Invalid seam, "
-                            + "consecutive horizontal indices are greater than one apart.");
-                }
-            }
-
-            Picture var5 = new Picture(var0.width() - 1, var0.height());
-
-            for (int var3 = 0; var3 < var0.height(); ++var3) {
-                int var4;
-                for (var4 = 0; var4 < var1[var3]; ++var4) {
-                    var5.set(var4, var3, var0.get(var4, var3));
-                }
-
-                for (var4 = var1[var3] + 1; var4 < var0.width(); ++var4) {
-                    var5.set(var4 - 1, var3, var0.get(var4, var3));
-                }
-            }
-            return var5;
-        }
+        this.colors = transpose(this.colors);
+        removeHorizontalSeam(seam);
+        this.colors = transpose(this.colors);
     }
 }
