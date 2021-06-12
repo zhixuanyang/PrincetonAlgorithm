12731a0261a6dd08c5414fff309ad6c5329906f7
zhixuanyang <yzx72497@gmail.com>
2021-06-12 12:40:40 +0800

    baseball update

--- Baseball/src/BaseballElimination.java
+++ Baseball/src/BaseballElimination.java
@@ -5,42 +5,44 @@ import edu.princeton.cs.algs4.FlowNetwork;
 import edu.princeton.cs.algs4.FordFulkerson;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.ArrayList;
 public class BaseballElimination {
     private int size;
     private int[] wins;
     private int[] loses;
     private int[] remaining;
-    private int high;
+    private boolean marked;
+    private ArrayList<String> teams;
     private int[][] eachother;
     private Map<String, Integer> teamlist;
     private FordFulkerson alog;
-    private FlowEdge edge;
     private FlowNetwork network;
+    private ArrayList<String> result;
     public BaseballElimination(String filename) {
         if (filename == null) {
             throw new IllegalArgumentException();
         }
         In input = new In(filename);
         size = input.readInt();
-        high = 0;
         teamlist = new HashMap<>();
         wins = new int[size];
         loses = new int[size];
+        teams = new ArrayList<>();
         remaining = new int[size];
         eachother = new int[size][size];
         for (int i = 0; i < size; i++) {
             String name = input.readString();
             teamlist.put(name, i);
+            teams.add(name);
             wins[i] = input.readInt();
-            if (wins[i] > high) {
-                high = wins[i];
-            }
             loses[i] = input.readInt();
             remaining[i] = input.readInt();
             for (int j = 0; j < size; j++) {
                 eachother[i][j] = input.readInt();
             }
         }
+        result = new ArrayList<>();
+        marked = false;
     }
 
     public int numberOfTeams() {
@@ -85,8 +87,19 @@ public class BaseballElimination {
         if (team == null || !teamlist.containsKey(team)) {
             throw new IllegalArgumentException();
         }
+        marked = false;
         int temp = teamlist.get(team);
-        if (wins[temp] + remaining[temp] < high) {
+        for (String name : teams) {
+            if (name == team) {
+                continue;
+            }
+            int id = teamlist.get(name);
+            if (wins[temp] + remaining[temp] < wins[id]) {
+                result.add(name);
+                marked = true;
+            }
+        }
+        if (marked) {
             return false;
         } else {
             return ffalog(temp);
@@ -94,27 +107,73 @@ public class BaseballElimination {
     }
 
     private boolean ffalog(int team) {
-        int totalgame = 0;
+        FlowEdge edge;
+        FlowEdge first;
+        FlowEdge second;
+        FlowEdge third;
+        int tempsize = calculate(size - 1 - 1);
+        int count = 0;
+        int total = 0;
+        network = new FlowNetwork(2 + size + tempsize);
         for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                if (i == team || j == team) {
+            if (i == team) {
+                continue;
+            }
+            for (int j = i + 1; j < size; j++) {
+                if (j == team) {
                     continue;
                 }
-                totalgame += 1;
+                count += 1;
+                edge = new FlowEdge(0, count, eachother[i][j]);
+                total += eachother[i][j];
+                first = new FlowEdge(count, tempsize + i + 1, Double.POSITIVE_INFINITY);
+                second = new FlowEdge(count, tempsize + j + 1, Double.POSITIVE_INFINITY);
+                network.addEdge(edge);
+                network.addEdge(first);
+                network.addEdge(second);
             }
         }
-        network = new FlowNetwork(2 + size - 1 + totalgame);
-        return true;
+        for (int i = 0; i < size; i++) {
+            if (i == team) {
+                continue;
+            }
+            third = new FlowEdge(tempsize + i + 1, 1 + size + tempsize,
+                    wins[team] + remaining[team] - wins[i]);
+            network.addEdge(third);
+        }
+        alog = new FordFulkerson(network, team, 2 + size + tempsize);
+        return !(total == (int) alog.value());
+    }
+
+    private int calculate(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return n + calculate(n - 1);
+        }
     }
     public Iterable<String> certificateOfElimination(String team) {
-        if (team == null) {
+        if (team == null || !teamlist.containsKey(team)) {
             throw new IllegalArgumentException();
         }
-        return null;
+        if (!isEliminated(team)) {
+            return null;
+        } else {
+            for (String name : teamlist.keySet()) {
+                if (name == team) {
+                    continue;
+                }
+                int id = teamlist.get(name);
+                if (alog.inCut(id)) {
+                    result.add(name);
+                }
+            }
+        }
+        return result;
     }
 
     public static void main(String[] args) {
-        /*BaseballElimination division = new BaseballElimination("teams5.txt");
+        BaseballElimination division = new BaseballElimination("teams5.txt");
         for (String team : division.teams()) {
             if (division.isEliminated(team)) {
                 StdOut.print(team + " is eliminated by the subset R = { ");
@@ -125,6 +184,6 @@ public class BaseballElimination {
             } else {
                 StdOut.println(team + " is not eliminated");
             }
-        }*/
+        }
     }
 }
