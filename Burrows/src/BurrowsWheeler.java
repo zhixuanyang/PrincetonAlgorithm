f5a9fbbe967d0f1232a0ec891584bc87196368a1
zhixuanyang <yzx72497@gmail.com>
2021-06-16 19:12:57 +0800

    initialized burrows

--- Burrows/src/BurrowsWheeler.java
+++ Burrows/src/BurrowsWheeler.java
@@ -18,10 +18,48 @@ public class BurrowsWheeler {
     }
 
     public static void inverseTransform() {
+        int start = BinaryStdIn.readInt();
+        String s = BinaryStdIn.readString();
+        int length = s.length();
+        char[] t = new char[length];
+        int[] temp = new int[256];
+        for (int i = 0; i < length; i++) {
+            char c = s.charAt(i);
+            t[i] = c;
+            temp[c] += 1;
+        }
 
+        int[] offset = new int[256];
+        int cum = 0;
+        for (int i = 1; i < 256; i++) {
+            cum += temp[i - 1];
+            offset[i] = cum;
+        }
+        int[] next = new int[length];
+        temp = new int[256];
+        for (int i = 0; i < length; i++) {
+            char c = t[i];
+            int alph = offset[c] + temp[c];
+            temp[c] += 1;
+            next[alph] = i;
+        }
+        int result = next[start];
+        for (int i = 0; i < length - 1; i++) {
+            BinaryStdOut.write(t[result]);
+            result = next[result];
+        }
+        BinaryStdOut.close();
+        BinaryStdOut.write(t[start]);
+        BinaryStdOut.close();
     }
 
     public static void main(String[] args) {
-
+        String par = args[0];
+        if (par.equals("-")) {
+            transform();
+        }
+        if (par.equals("+")) {
+            inverseTransform();
+        }
     }
 }
--- Burrows/src/MoveToFront.java
+++ Burrows/src/MoveToFront.java
@@ -1,13 +1,43 @@
+import edu.princeton.cs.algs4.BinaryStdOut;
+import edu.princeton.cs.algs4.BinaryStdIn;
+import java.util.LinkedList;
 public class MoveToFront {
-    public static void encode() {
 
+    public static void encode() {
+        LinkedList<Character> list = new LinkedList<>();
+        for (int i = 0; i < 256; i++) {
+            list.add((char) i);
+        }
+        while (!BinaryStdIn.isEmpty()) {
+            char c = BinaryStdIn.readChar();
+            int index = list.indexOf(c);
+            BinaryStdOut.write(index, 8);
+            list.remove(index);
+            list.addFirst(c);
+        }
+        BinaryStdOut.close();
     }
 
     public static void decode() {
-
+        LinkedList<Character> list = new LinkedList<>();
+        for (int i = 0; i < 256; i++) {
+            list.add((char) i);
+        }
+        while (!BinaryStdIn.isEmpty()) {
+            char index = BinaryStdIn.readChar();
+            char c = list.get(index);
+            BinaryStdOut.write(c);
+            list.remove(index);
+            list.addFirst(c);
+        }
+        BinaryStdOut.close();
     }
 
     public static void main(String[] args) {
-
+        if (("-").equals(args[0])) {
+            encode();
+        } else if (("+").equals(args[0])) {
+            decode();
+        }
     }
 }
